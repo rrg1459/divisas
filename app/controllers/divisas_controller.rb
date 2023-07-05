729551d5347b719e32769eab77fcf407d664b30f
rafaDev <rrg1459@hotmail.com>
2023-07-04 20:54:02 -0400

    to round 2

--- app/controllers/divisas_controller.rb
+++ app/controllers/divisas_controller.rb
@@ -5,8 +5,8 @@ class DivisasController < ApplicationController
 
   def scraping
     page = Nokogiri.HTML(open('https://www.bcv.org.ve/'))
-    dolar = page.at_css('[id="dolar"]').at_css('[class="col-sm-6 col-xs-6 centrado"]').text.strip.gsub!(',','.').to_f.round(4)
-    euro = page.at_css('[id="euro"]').at_css('[class="col-sm-6 col-xs-6 centrado"]').text.strip.gsub!(',','.').to_f.round(4)
+    dolar = page.at_css('[id="dolar"]').at_css('[class="col-sm-6 col-xs-6 centrado"]').text.strip.gsub!(',','.').to_f.round(2)
+    euro = page.at_css('[id="euro"]').at_css('[class="col-sm-6 col-xs-6 centrado"]').text.strip.gsub!(',','.').to_f.round(2)
     date = page.at_css('[class="date-display-single"]').text
     render json: { date: date, dolar: dolar, euro: euro }
   end
