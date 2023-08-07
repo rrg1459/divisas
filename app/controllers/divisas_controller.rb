a3b1dfb9b380f4a233b97b01cc91cfb6932888e3
rafaDev <rrg1459@hotmail.com>
2023-08-07 17:14:05 -0400

    uncomment

--- app/controllers/divisas_controller.rb
+++ app/controllers/divisas_controller.rb
@@ -7,20 +7,20 @@ class DivisasController < ApplicationController
 
   def scraping
     # sleep 1
-    # render json: { date: "Jueves, 10 Agosto  1967", dolar: 18.16, euro: 20.7 }
+    render json: { date: "Jueves, 10 Agosto  1967", dolar: 18.16, euro: 20.7 }
     # page = Nokogiri.HTML(open('https://www.bcv.org.ve/'))
     # dolar = page.at_css('[id="dolar"]').at_css('[class="col-sm-6 col-xs-6 centrado"]').text.strip.gsub!(',','.').to_f.truncate(2)
     # euro = page.at_css('[id="euro"]').at_css('[class="col-sm-6 col-xs-6 centrado"]').text.strip.gsub!(',','.').to_f.truncate(2)
     # date = page.at_css('[class="date-display-single"]').text
     # render json: { date: date, dolar: dolar, euro: euro }
 
-    @date = "Jueves, 10 Agosto  1967"
-    @dolar = 18.16
-    @euro = 20.7
+  #   @date = "Jueves, 10 Agosto  1967"
+  #   @dolar = 18.16
+  #   @euro = 20.7
 
-  respond_to do |format|
-    format.json
-  end
+  # respond_to do |format|
+  #   format.json
+  # end
 
 
   end
